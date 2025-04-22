7adfd82a4a56f694273df97f1e381f42354cef2b
Shilpa Bindhu <shilpamaalootty@gmail.com>
2025-04-22 21:01:50 +0200

    statusOptions rendered

--- router/habit.js
+++ router/habit.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Habit = require("../models/Habit");
+const { HABIT_STATUS_OPTIONS } = require("../constants/habitStatus");
 
 //get
 router.get("/", async (req, res) => {
@@ -12,12 +13,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/status-options", (req, res) => {
+  res.status(200).json(Object.values(HABIT_STATUS_OPTIONS));
+});
+
 //post
 router.post("/", async (req, res) => {
   try {
     const newHabit = new Habit(req.body);
     await newHabit.save();
     res.status(201).json(newHabit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedHabit = await Habit.findByIdAndDelete(req.params.id);
+    if (!deletedHabit)
+      return res.status(404).json({ message: "couldn't find habit" });
+
+    res.status(200).json(deletedHabit);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
